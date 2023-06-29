afcea3930b9d4f9438f4923f68ca085a52a56d79
Jamie Brassel <jbras3@gmail.com>
2023-06-29 17:15:54 -0400

    add list videos example

---
+++ examples/list_videos.py
@@ -0,0 +1,26 @@
+from http.cookiejar import MozillaCookieJar
+from pprint import pprint
+
+from ytstudio.ytstudio import Studio
+from ytstudio.ytstudio.templates import ALL_TRUE
+
+jar = MozillaCookieJar('cookies.txt')
+jar.load()
+with open('token') as fp:
+    token = fp.read()
+
+with Studio(jar, token) as studio:
+    studio.login()
+
+    # get 10 videos, but do not retrieve any attributes
+    videos = studio.list_videos(10)
+    pprint(videos)
+
+    # get 50 videos, and retrieve scheduling information, titles, and video IDs
+    videos = studio.list_videos(
+        50,
+        scheduledPublishingDetails=ALL_TRUE,
+        title=True,
+        videoId=True
+    )
+    pprint(videos)
