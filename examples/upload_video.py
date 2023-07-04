3fe7671ba82241d8269bdbbbbc92a0478675b577
Jamie Brassel <jbras3@gmail.com>
2023-07-04 18:17:45 -0400

    update upload_video.py

--- examples/upload_video.py
+++ examples/upload_video.py
@@ -1,26 +1,44 @@
-from ytstudio import Studio
-import asyncio
-import os
-import json
+from http.cookiejar import MozillaCookieJar
+from os.path import getsize
+from typing import TYPE_CHECKING, cast
 
+from tqdm import tqdm
+from tqdm.utils import CallbackIOWrapper
 
-def progress(yuklenen, toplam):
-    print(f"{round(yuklenen / toplam) * 100}% upload", end="\r")
-    pass
+from ytstudio.ytstudio import Studio
+from ytstudio.ytstudio.typing import Visibility
 
+if TYPE_CHECKING:
+    from _typeshed import SupportsRead
 
-if os.path.exists("./login.json"):
-    LOGIN_FILE = json.loads(open("./login.json", "r").read())
-else:
-    exit("can't run example without login json")
+jar = MozillaCookieJar('cookies.txt')
+jar.load()
+with open('token') as fp:
+    token = fp.read()
 
-yt = Studio(LOGIN_FILE)
+file = 'test_video.mp4'
+with Studio(jar, token) as studio:
+    # upload file without progress bar and using default fields
+    with open(file, 'rb') as data:  # open as byte stream to prevent entire file from being read into memory
+        video_id = studio.upload_video(data)
+        print(f'Successfully uploaded! videoId: {video_id}')
 
+    # upload file with tqdm progress bar and using default fields
+    with open(file, 'rb') as stream, tqdm(total=getsize(file), unit_scale=True) as pbar:
+        data = CallbackIOWrapper(pbar.update, stream)
+        data = cast(SupportsRead[bytes], data)  # optionally tell type checkers that data supports read()
+        video_id = studio.upload_video(data)
+        print(f'Successfully uploaded! videoId: {video_id}')
 
-async def main():
-    await yt.login()
-    sonuc = await yt.uploadVideo(os.path.join(os.getcwd(), "test_video.mp4"), progress=progress)
-    print(f"successfully uploaded! videoId: {sonuc['videoId']}")
+    # upload file with tqdm progress bar and using custom fields
+    with open(file, 'rb') as stream, tqdm(total=getsize(file), unit_scale=True) as pbar:
+        data = CallbackIOWrapper(pbar.update, stream)
+        data = cast(SupportsRead[bytes], data)  # optionally tell type checkers that data supports read()
+        video_id = studio.upload_video(data, 'New title', 'New description\nAnd stuff!', Visibility.UNLISTED, draft=False)
+        print(f'Successfully uploaded! videoId: {video_id}')
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+    # upload arbitrary data stream with tqdm progress bar
+    random_data = 'aaaaaaaaaaa BBBBBBBBBBBBBBB ccccccccccccccc'
+    with tqdm(random_data) as data:
+        video_id = studio.upload_video(random_data)
+        print(f'Successfully uploaded! videoId: {video_id}')
