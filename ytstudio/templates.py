d0269512d2fb16e1c28b7c85b642f88c898fcbc0
Jamie Brassel <jbras3@gmail.com>
2023-07-06 17:39:11 -0400

    remove more unnecessary data from templates

--- ytstudio/templates.py
+++ ytstudio/templates.py
@@ -3,32 +3,22 @@ from typing import Any
 ALL_TRUE = dict(all=True)
 
 EMPTY_DICT = {}
-EMPTY_LIST = []
 CONTEXT: dict[str, Any] = dict(
     context=dict(
-        clientScreenNonce='',
+        request=dict(sessionInfo=EMPTY_DICT),
         user=EMPTY_DICT,
         client=dict(
             clientName=62,
-            clientVersion='1.20230621.01.01',
-            experimentsToken='',
-            gl='US',
-            hl='en',
-            utcOffsetMinutes=-240
-        ),
-        request=dict(
-            internalExperimentFlags=EMPTY_LIST,
-            returnLogEntry=True,
-            sessionInfo=EMPTY_DICT
+            clientVersion='1.20230621.01.01'
         )
     )
 )
 
-CREATE_PLAYLIST: dict[str, Any] = dict(**CONTEXT)
-LIST_PLAYLISTS: dict[str, Any] = dict(**CONTEXT)
-LIST_VIDEOS: dict[str, Any] = dict(**CONTEXT)
-METADATA_UPDATE: dict[str, Any] = dict(**CONTEXT)
-UPLOAD_VIDEO: dict[str, Any] = dict(**CONTEXT)
+CREATE_PLAYLIST = CONTEXT.copy()
+LIST_PLAYLISTS = CONTEXT.copy()
+LIST_VIDEOS = CONTEXT.copy()
+METADATA_UPDATE = CONTEXT.copy()
+UPLOAD_VIDEO = CONTEXT.copy()
 
 
 def generate(token: Any, channel_id: Any, on_behalf_of_user: Any) -> None:
