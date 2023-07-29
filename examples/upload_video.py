177cde4701cf91bad4e4c24319fc620ce867aff7
Jamie Brassel <jbras3@gmail.com>
2023-07-28 20:54:44 -0400

    update upload_video.py to use rich for simplicity and actually work

--- examples/upload_video.py
+++ examples/upload_video.py
@@ -1,15 +1,9 @@
 from json import load
-from os.path import getsize
-from typing import TYPE_CHECKING, cast
 
-from tqdm import tqdm
-from tqdm.utils import CallbackIOWrapper
+from rich import progress
 
 from ytstudio.ytstudio import Studio
 from ytstudio.ytstudio.typing import Visibility
-
-if TYPE_CHECKING:
-    from _typeshed import SupportsRead
 
 file = 'test_video.mp4'
 
@@ -22,22 +16,17 @@ with Studio(cookies) as studio:
         video_id = studio.upload_video(data)
         print(f'Successfully uploaded! videoId: {video_id}')
 
-    # upload file with tqdm progress bar and using default fields
-    with open(file, 'rb') as stream, tqdm(total=getsize(file), unit_scale=True) as pbar:
-        data = CallbackIOWrapper(pbar.update, stream)
-        data = cast(SupportsRead[bytes], data)  # optionally tell type checkers that data supports read()
+    # upload file with rich progress bar and using default fields
+    with progress.open(file, 'rb') as data:
         video_id = studio.upload_video(data)
         print(f'Successfully uploaded! videoId: {video_id}')
 
-    # upload file with tqdm progress bar and using custom fields
-    with open(file, 'rb') as stream, tqdm(total=getsize(file), unit_scale=True) as pbar:
-        data = CallbackIOWrapper(pbar.update, stream)
-        data = cast(SupportsRead[bytes], data)  # optionally tell type checkers that data supports read()
+    # upload file with rich progress bar and using custom fields
+    with progress.open(file, 'rb') as data:
         video_id = studio.upload_video(data, 'New title', 'New description\nAnd stuff!', Visibility.UNLISTED, draft=False)
         print(f'Successfully uploaded! videoId: {video_id}')
 
-    # upload arbitrary data stream with tqdm progress bar
-    random_data = 'aaaaaaaaaaa BBBBBBBBBBBBBBB ccccccccccccccc'
-    with tqdm(random_data) as data:
-        video_id = studio.upload_video(random_data)
-        print(f'Successfully uploaded! videoId: {video_id}')
+    # upload arbitrary data stream
+    data = 'aaaaaaaaaaa BBBBBBBBBBBBBBB ccccccccccccccc'
+    video_id = studio.upload_video(data)
+    print(f'Successfully uploaded! videoId: {video_id}')
